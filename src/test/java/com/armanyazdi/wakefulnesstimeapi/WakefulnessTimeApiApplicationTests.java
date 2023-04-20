2342a20220fa851b2ce83c3f31905037d19cc6c9
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-20 13:50:58 +0330

    Add Tests

--- src/test/java/com/armanyazdi/wakefulnesstimeapi/WakefulnessTimeApiApplicationTests.java
+++ src/test/java/com/armanyazdi/wakefulnesstimeapi/WakefulnessTimeApiApplicationTests.java
@@ -3,11 +3,28 @@ package com.armanyazdi.wakefulnesstimeapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class WakefulnessTimeApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    public void testCaffeineContentOfDrinks() {
+        Drink.addData("americano", "now");
+        assertEquals(249, Drink.caffeine);
     }
 
+    @Test
+    public void testMinAndMaxWakefulnessTimeOfDrinks13To17() {
+        Drink.addData("americano", "13:00");
+        assertEquals(11, Drink.min);
+        assertEquals(14, Drink.max);
+    }
+
+    @Test
+    public void testMinAndMaxWakefulnessTimeOfDrinksAtOtherTimes() {
+        Drink.addData("americano", "12:59");
+        assertEquals(10, Drink.min);
+        assertEquals(13, Drink.max);
+    }
 }
