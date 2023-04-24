ed1d67c22ae82d1cfa81b145ea7d6421f4c1d832
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-24 20:48:41 +0330

    Update WakefulnessTime.java

--- src/main/java/com/armanyazdi/wakefulnesstimeapi/WakefulnessTime.java
+++ src/main/java/com/armanyazdi/wakefulnesstimeapi/WakefulnessTime.java
@@ -8,24 +8,28 @@ public class WakefulnessTime {
     private final Long id;
     private final HttpStatus status;
     private final String time;
-    private final String drink;
+    private final String coffee;
+    private final String arabica;
+    private final String robusta;
     private final String caffeine;
     private final String wakefulness;
     private final String min;
     private final String max;
     private final String avg;
 
-    public WakefulnessTime(Long id, HttpStatus status, String drink, String time) {
-        Drink.addData(drink, time);
+    public WakefulnessTime(Long id, HttpStatus status, String coffee, String robusta, String time) {
+        Coffee.calculate(coffee, robusta, time);
 
         this.id = id;
         this.status = status;
         this.time = "%s:%s".formatted(String.format("%02d", Time.fix(time).getHourOfDay()), String.format("%02d", Time.fix(time).getMinuteOfHour()));
-        this.drink = drink.substring(0, 1).toUpperCase() + drink.substring(1);
-        this.caffeine = "%s mg".formatted(Drink.caffeine);
-        this.wakefulness = "%s-%s hrs".formatted(Drink.min, Drink.max);
-        this.min = Time.format(Time.fix(time), Drink.min, 15);
-        this.max = Time.format(Time.fix(time), Drink.max, -15);
-        this.avg = Time.format(Time.fix(time), (Drink.min + Drink.max) / 2, 10);
+        this.coffee = coffee.substring(0, 1).toUpperCase() + coffee.substring(1);
+        this.arabica = Coffee.arabica + "%";
+        this.robusta = Coffee.robusta + "%";
+        this.caffeine = "%s mg".formatted(Coffee.caffeine);
+        this.wakefulness = "%s-%s hours".formatted(Coffee.min, Coffee.max);
+        this.min = Time.format(Time.fix(time), Coffee.min, 15);
+        this.max = Time.format(Time.fix(time), Coffee.max, -15);
+        this.avg = Time.format(Time.fix(time), (Coffee.min + Coffee.max) / 2, 10);
     }
 }
