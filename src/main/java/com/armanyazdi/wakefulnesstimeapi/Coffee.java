c8dc9d8e65b37a7a125e8b0b1d4b28b0fde1503a
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-24 20:47:24 +0330

    Update Coffee.java

---
+++ src/main/java/com/armanyazdi/wakefulnesstimeapi/Coffee.java
@@ -0,0 +1,33 @@
+package com.armanyazdi.wakefulnesstimeapi;
+
+public class Coffee {
+    public static short content;
+    public static int arabica, robusta, caffeine, min, max;
+
+    public static void calculate(String coffee, String robusta, String time) {
+        switch (coffee.toLowerCase()) {
+            case "americano" -> content = 249;
+            case "espresso" -> content = 126;
+            case "latte" -> content = 154;
+            case "mocha" -> content = 152;
+            case "cappuccino" -> content = 173;
+            case "frappuccino" -> content = 79;
+            case "macchiato" -> content = 85;
+            case "affogato" -> content = 85;
+            case "chemex" -> content = 172;
+            case "brewed" -> content = 96;
+            case "nescafe" -> content = 50;
+        }
+
+        Coffee.robusta = Integer.parseInt(robusta.replaceAll("[^0-9]",""));
+        arabica = 100 - Coffee.robusta;
+        caffeine = content + (Coffee.robusta * (content / 100));
+        min = Math.round((float) caffeine / 25);
+        max = min + 3;
+
+        if (Time.fix(time).getHourOfDay() >= 13 && Time.fix(time).getHourOfDay() < 17) {
+            min += 1;
+            max += 1;
+        }
+    }
+}
