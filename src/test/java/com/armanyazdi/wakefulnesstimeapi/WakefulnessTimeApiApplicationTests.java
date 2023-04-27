cd3d1fb76b18d19bd90c44524b349ffb062a26f8
Arman Yazdi <f41c0n.32@gmail.com>
2023-04-27 17:07:50 +0330

    Update tests

--- src/test/java/com/armanyazdi/wakefulnesstimeapi/WakefulnessTimeApiApplicationTests.java
+++ src/test/java/com/armanyazdi/wakefulnesstimeapi/WakefulnessTimeApiApplicationTests.java
@@ -10,34 +10,34 @@ class WakefulnessTimeApiApplicationTests {
 
     @Test
     public void testCaffeineContentOfCoffee() {
-        Coffee.calculate("americano", "0", "now");
+        Coffee.calculateCaffeine("americano", "0", "now");
         assertEquals(249, Coffee.caffeine);
 
-        Coffee.calculate("americano", "20", "now");
+        Coffee.calculateCaffeine("americano", "20", "now");
         assertEquals(289, Coffee.caffeine);
 
-        Coffee.calculate("americano", "30", "now");
+        Coffee.calculateCaffeine("americano", "30", "now");
         assertEquals(309, Coffee.caffeine);
     }
 
     @Test
     public void testMinAndMaxWakefulnessTimeOfCoffee13To17() {
-        Coffee.calculate("americano", "0", "13:00");
+        Coffee.calculateCaffeine("americano", "0", "13:00");
         assertEquals(11, Coffee.min);
         assertEquals(14, Coffee.max);
 
-        Coffee.calculate("americano", "20", "14:30");
+        Coffee.calculateCaffeine("americano", "20", "14:30");
         assertEquals(13, Coffee.min);
         assertEquals(16, Coffee.max);
     }
 
     @Test
     public void testMinAndMaxWakefulnessTimeOfCoffeeAtOtherTimes() {
-        Coffee.calculate("americano", "0", "12:59");
+        Coffee.calculateCaffeine("americano", "0", "12:59");
         assertEquals(10, Coffee.min);
         assertEquals(13, Coffee.max);
 
-        Coffee.calculate("americano", "20", "7:30");
+        Coffee.calculateCaffeine("americano", "20", "7:30");
         assertEquals(12, Coffee.min);
         assertEquals(15, Coffee.max);
     }
